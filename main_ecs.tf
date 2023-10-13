9a68e972deedb217e19e907f1f4c67f68b9dd38b
snacks <kjyhi9981@gmail.com>
2023-10-13 10:30:58 +0900

    main_ecs.tf 업로드

---
+++ main_ecs.tf
@@ -0,0 +1,136 @@
+module "ecs" {
+  source  = "terraform-aws-modules/ecs/aws"
+  version = "~> 5.2"
+
+  cluster_name = local.name
+
+  cluster_service_connect_defaults = {
+    namespace = aws_service_discovery_private_dns_namespace.this.arn
+  }
+
+  # fargate_capacity_providers = {
+  #   FARGATE      = {}
+  #   FARGATE_SPOT = {}
+  # }
+  
+fargate_capacity_providers = {
+    FARGATE = {
+      default_capacity_provider_strategy = {
+        weight = 50
+        base   = 20
+      }
+    }
+    FARGATE_SPOT = {
+      default_capacity_provider_strategy = {
+        weight = 50
+      }
+    }
+  }
+
+services = {
+    ecsdemo-frontend = {
+      cpu    = 1024
+      memory = 4096
+
+      # Container definition(s)
+      container_definitions = {
+         
+        fluent-bit = {
+          cpu       = 512
+          memory    = 1024
+          essential = true
+          image     = nonsensitive(data.aws_ssm_parameter.fluentbit.value)
+          firelens_configuration = {
+            type = "fluentbit"
+          }
+          memory_reservation = 50
+        }
+
+    (local.container_name) = {
+          cpu       = 512
+          memory    = 1024
+          essential = true
+          image     = "475697618750.dkr.ecr.ap-northeast-2.amazonaws.com/1-nginx:latest"
+          port_mappings = [
+            {
+              name          = local.container_name
+              containerPort = local.container_port
+              hostPort      = local.container_port
+              protocol      = "tcp"
+            }
+          ]
+          # public.ecr.aws/aws-containers/ecsdemo-frontend:776fd50
+          # Example image used requires access to write to root filesystem
+          readonly_root_filesystem = false
+
+         dependencies = [{
+            containerName = "fluent-bit"
+            condition     = "START"
+          }]
+
+          enable_cloudwatch_logging = false
+          log_configuration = {
+            logDriver = "awsfirelens"
+            options = {
+              Name                    = "firehose"
+              region                  = local.region
+              delivery_stream         = "my-stream"
+              log-driver-buffer-limit = "2097152"
+            }
+          }
+          memory_reservation = 100
+        }
+      }
+
+      service_connect_configuration = {
+        namespace = aws_service_discovery_http_namespace.this.arn
+        service = {
+          client_alias = {
+            port     = local.container_port
+            dns_name = local.container_name
+          }
+          port_name      = local.container_name
+          discovery_name = local.container_name
+        }
+      }
+
+     load_balancer = {
+        service = {
+          target_group_arn = element(module.alb.target_group_arns, 0)
+          container_name   = local.container_name
+          container_port   = local.container_port
+        }
+      }
+
+      subnet_ids = module.vpc.private_subnets
+      security_group_rules = {
+        alb_ingress_3000 = {
+          type                     = "ingress"
+          from_port                = local.container_port
+          to_port                  = local.container_port
+          protocol                 = "tcp"
+          description              = "Service port"
+          source_security_group_id = module.alb_sg.security_group_id
+        }
+        egress_all = {
+          type        = "egress"
+          from_port   = 0
+          to_port     = 0
+          protocol    = "-1"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      }
+    }
+  }
+
+  
+
+  # Shared task execution role
+  create_task_exec_iam_role = true
+  # Allow read access to all SSM params in current account for demo
+  task_exec_ssm_param_arns = ["arn:aws:ssm:${local.region}:${data.aws_caller_identity.current.account_id}:parameter/*"]
+  # Allow read access to all secrets in current account for demo
+  task_exec_secret_arns = ["arn:aws:secretsmanager:${local.region}:${data.aws_caller_identity.current.account_id}:secret:*"]
+
+  tags = local.tags
+}
